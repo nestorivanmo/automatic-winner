c19fb5377a538ef96b024a052920f43aae45a947
nestorivanmo <nestor.martinez.98@hotmail.com>
2019-10-26 10:27:53 -0500

    started new project

---
+++ AutomaticWinner/AutomaticWinner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutomaticWinner
+//
+//  Created by Néstor I. Martínez Ostoa on 26/10/19.
+//  Copyright © 2019 Néstor I. Martínez Ostoa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
